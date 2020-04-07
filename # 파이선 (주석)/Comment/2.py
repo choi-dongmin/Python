909862e8d88a55badd2252dad66cd958041a3cfd
choi-dongmin <57117748+choi-dongmin@users.noreply.github.com>
2020-04-07 19:19:49 +0200

    Add files via upload

---
+++ # 파이선 (주석)/Comment/2.py
@@ -0,0 +1,12 @@
+input = 33
+real_me = 11
+real_you = "ab"
+
+if real_me == input:
+	print("Hello me")
+elif real_you == input:
+	print ("Hello you")
+else:
+	print("Try again")
+
+# 위 로직은 로그인을 하기위한 로직입니다.
