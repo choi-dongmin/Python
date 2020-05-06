997b7e25aedce70f7c581e5adce9c6e76cb27e98
choi-dongmin <57117748+choi-dongmin@users.noreply.github.com>
2020-05-06 15:24:12 +0200

    Add files via upload

---
+++ # 객체지향/Class/1.py
@@ -0,0 +1,5 @@
+class Cal(object):
+    def __init__(self, v1, v2):
+        print(v1, v2)
+
+c1 = Cal(10,10)
---
+++ # 객체지향/Class/2.py
@@ -0,0 +1,17 @@
+cclass Cal (object):
+	def __init__(self,v1, v2):
+		self.v1 = v1
+		self.v2 = v2
+
+	def add ():
+		return v1 + v2
+	def subtarct():
+		return v1 - v2
+
+c1 = Cal(10, 10)
+print(c1.add())
+print(c1.subtract())
+
+c2 = Cal(30,20)
+print(c2.add())
+print(c2.subtarct())
